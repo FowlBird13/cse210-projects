29e35689837499f6611a781b5d2ec7ff7339c3df
FowlBird13 <bradleympeterson05@gmail.com>
2026-04-23 14:22:56 -0600

    finished prep 01 assignment

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,16 @@
+// Incredible code created by Bradley Mark Peterson on 4/23/26 for cs210.
+// There was something else I was supposed to put in this header comment
+// but I can't remember what...
 using System;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.Write("What's your first name? ");
+        string bmpfirstName = Console.ReadLine();
+        Console.Write("What's your last name? ");
+        string bmplastName = Console.ReadLine();
+        Console.WriteLine($"Your name is {bmplastName}, {bmpfirstName} {bmplastName}.");
     }
 }
