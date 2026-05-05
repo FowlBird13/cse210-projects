47f1228b31b76c4cf9cff91a39a5880ffd25521b
FowlBird13 <bradleympeterson05@gmail.com>
2026-05-05 14:36:49 -0600

    finished prep 5

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,55 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        //Print a nice welcome message to the console.
+        static void BmpDisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the program!");
+        }
+
+        //Ask for and return the user's name.
+        static string BmpPromptUserName()
+        {
+            Console.Write("Please enter your name: ");
+            string bmpName = Console.ReadLine();
+            return bmpName;
+        }
+
+        //Ask for and return the user's favorite number.
+        static int BmpPromptUserNumber()
+        {
+            Console.Write("Please enter your favorite number: ");
+            return int.Parse(Console.ReadLine());
+        }
+
+        //Accepts an out integer parameter, prompts the user for their birth year,
+        //then sets the parameter to that year.
+        static void BmpPromptUserBirthYear(out int year)
+        {
+            Console.Write("Please enter the year you were born: ");
+            year = int.Parse(Console.ReadLine());
+        }
+
+        //Square the inputed number
+        static int BmpSquareNumber(int bmpNum)
+        {
+            return bmpNum * bmpNum;
+        }
+
+        //Dsiplay the results of the functions created earlier
+        static void BmpDisplayResult(string bmpName, int bmpSquaredNum, int bmpBirthYear)
+        {
+            Console.WriteLine($"{bmpName}, the square of your number is: {bmpSquaredNum}");
+            Console.WriteLine($"{bmpName}, you will turn {2026 - bmpBirthYear} this year.");
+        }
+
+        //Pull the lever Kronk!
+        BmpDisplayWelcome();
+        string bmpName = BmpPromptUserName();
+        int bmpFavNum = BmpPromptUserNumber();
+        int bmpBirthYear;
+        BmpPromptUserBirthYear(out bmpBirthYear);
+        int bmpSquaredNum = BmpSquareNumber(bmpFavNum);
+        BmpDisplayResult(bmpName, bmpSquaredNum, bmpBirthYear);
     }
 }
